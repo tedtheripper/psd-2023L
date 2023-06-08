822553bed4b258fde26f20795c0ba01773d5d5e5
tedtheripper <lemon.juice0731@gmail.com>
2023-06-08 12:18:07 +0200

    regenerating data instead of clipping

--- utils.py
+++ utils.py
@@ -29,10 +29,13 @@ def rate_to_dict(rate: RateOfReturn) -> Dict[str, any]:
 
 def generate_rate_of_return(investment_params: InvestmentParameters) -> RateOfReturn:
     mu = investment_params.mu
-    sigma = np.sqrt(investment_params.sigma_sq) * 10e-3
+    sigma = np.sqrt(investment_params.sigma_sq)
 
-    normal_data = np.random.normal(mu, sigma)
-    rate_value = np.clip(normal_data, -0.1, 0.1)
+    rate_value = np.random.normal(mu, sigma)
+    
+    while abs(rate_value) > 0.1:
+        rate_value = np.random.normal(mu, sigma)
+    
     curr_timestamp = datetime.timestamp(datetime.now())
     return RateOfReturn(investment_params.name, rate_value, curr_timestamp)
 
